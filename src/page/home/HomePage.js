e6d77089d508355d28bff21fa7286b86d0bc1681
Trương Cao Thành Nhật <92218298+TruongCaoThanhNhat@users.noreply.github.com>
2023-06-16 09:31:10 +0700

    sua giao dien trang chu

--- src/page/home/HomePage.js
+++ src/page/home/HomePage.js
@@ -3,6 +3,7 @@ import "./home.scss";
 import { fetchRssFeed } from "../../util/RssFeed";
 import Article from "../../components/article/Article";
 import HeaderLayout from "../../layout/HeaderLayout";
+import ArticleTitle from "../../components/article/ArticleTitle";
 
 
 const HomePage = () => {
@@ -22,23 +23,40 @@ const HomePage = () => {
   }, []);
 
   // Chia dữ liệu thành 2 phần
-  const halfIndex = Math.ceil(dataArticle.length / 2);
-  const firstHalf = dataArticle.slice(0, halfIndex);
-  const secondHalf = dataArticle.slice(halfIndex);
-  console.log(firstHalf)
+  const firstSection = dataArticle.slice(0, 25);
+  const secondSection = dataArticle.slice(25, 50);
+  const thirdSection = dataArticle.slice(50, 100);
+  console.log(firstSection)
 
 
   return (
     <div className="home">
+      <HeaderLayout></HeaderLayout>
+      <h3 className="caption">Trang chủ</h3>
       <div className="layout">
-        <HeaderLayout></HeaderLayout>
-        <h3 className="caption">Trang chủ</h3>
-        <section className="list-home">
+        <section className="list-home first">
           {/* <ListArticleHome></ListArticleHome> */}
           {
-            dataArticle.map((item, index) => (
+            firstSection.map((item, index) => (
               <div key={index}>
                 <Article feed={item}></Article>
+              </div>
+            ))
+          }
+          {
+            secondSection.map((item, index) => (
+              <div key={index}>
+                <Article feed={item}></Article>
+              </div>
+            ))
+          }
+        </section>
+
+        <section className="article-right">
+          {
+            thirdSection.map((item, index) => (
+              <div key={index}>
+                <ArticleTitle feed={item}></ArticleTitle>
               </div>
             ))
           }
